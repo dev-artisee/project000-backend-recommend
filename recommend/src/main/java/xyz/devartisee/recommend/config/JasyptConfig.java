3d14c4a4ce1e7769e68e0448d63f9d0f32f56369
wonsic3686@gmail.com <wonsic3686@gmail.com>
2023-10-24 03:34:33 +0900

    jasypt 추가

--- recommend/src/main/java/xyz/devartisee/recommend/config/JasyptConfig.java
+++ recommend/src/main/java/xyz/devartisee/recommend/config/JasyptConfig.java
@@ -19,11 +19,11 @@ public class JasyptConfig {
     public StringEncryptor stringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(password);
+        config.setPassword("jasyptproject");
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
-        config.setProviderName("wonsic3686");
+//        config.setProviderName("wonsic3686");
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
         config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
         config.setStringOutputType("base64");
