d44823db852462a895ffdb27650aabe4da12e08a
정원식 <wsjeong@nm-partner.com>
2023-11-01 18:44:24 +0900

    feat: 카테고리 장소 검색 서비스 추가 2

--- recommend/src/main/java/xyz/devartisee/recommend/service/MapServiceImpl.java
+++ recommend/src/main/java/xyz/devartisee/recommend/service/MapServiceImpl.java
@@ -1,40 +1,50 @@
 package xyz.devartisee.recommend.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import xyz.devartisee.recommend.service.dto.exapi.getCategoryResponse.GetCategoryResponse;
 import xyz.devartisee.recommend.service.dto.request.GetPlaceRequest;
 import xyz.devartisee.recommend.service.dto.response.GetPlaceResponse;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 @RequiredArgsConstructor
 @Transactional
 @Service
 public class MapServiceImpl implements MapService {
 
+    WebClient webClient = WebClient
+            .builder()
+            .baseUrl("https://dapi.kakao.com")
+            .defaultHeader(HttpHeaders.AUTHORIZATION, "KakaoAK eac97934f12a27f83e1c9cdd30f46041")
+            .build();
+
     @Override
     public List<GetPlaceResponse> getPlaceList(GetPlaceRequest request) {
 
-//        GetCategoryResponse getCategoryResponse;
-//
-//        WebClient webClient = WebClient.create("https://dapi.kakao.com");
-//        webClient.get().uri( uriBuilder -> uriBuilder.path("/v2/local/search/category.json")
-//                .queryParam("category_group_code", "FD6")
-//                .queryParam("x", request.getLongitude().toString())
-//                .build()
-//        ).exchangeToMono(clientResponse -> {
-//            if (clientResponse.statusCode().equals(HttpStatus.OK)) {
-//                getCategoryResponse = clientResponse.bodyToMono(GetCategoryResponse.class);
-//            }
-//        })
+        Mono<GetCategoryResponse> getCategoryResponse = null;
 
-
-
-
+        webClient.get().uri( uriBuilder -> uriBuilder.path("/v2/local/search/category.json")
+                .queryParam("category_group_code", "FD6")
+                .queryParam("x", request.getLongitude().toString())
+                .queryParam("y", request.getLatitude().toString())
+                .queryParam("radius", request.getRadius().toString())
+                .build()
+        ).exchangeToMono(clientResponse -> {
+            if (clientResponse.statusCode().equals(HttpStatus.OK)) {
+                 getCategoryResponse = clientResponse.bodyToMono(GetCategoryResponse.class).block();
+            } else {
+                // log.warn("{} API Error: {} {}; params: {};", this, response.statusCode(), response.statusCode().getReasonPhrase(), formData);
+                return null;
+            }
+            return null;
+        });
         return null;
     }
     // 카테고리로 장소 검색하기 - 카테고리 그룹 코드
